45eba656a02ec8d663f60252574428778b11fff3
Lucas Burch <lucasburch@hotmail.com>
2022-06-27 18:18:53 -0300

    Aula 4 - Padrões de estilização
    Primeiramente, adicionamos as fontes via google fonts e vimos como utilizar o normalize para facilitar o reset de estilos. O recomendável é puxar isso com @import-normalize no arquivo scss, mas quando não funcionar - como neste curso - a gente puxa no terminal:
    
    "npm install normalize.css"
    Depois importamos direto no index.tsx lá de src.
    
    Montamos também o header do site, pegando uma estilização pronta e criando dois arquivos importantes para boas práticas: _variaveis e _breakpoints. O primeiro com cores e paddings padrões para a página e o segundo com 4 pontos de mudança diferentes correspondentes à responsividade da página.
    
    Por fim, montamos dentro do componente do Cardapio um componente de Buscador. Nele, voltamos a utilizar declarações destructuring de JS/React no início do componente Cardapio com "const [busca, setBusca] = useState("") ". Criamos uma interface dentro do componente de função Buscador com suas propriedades (onde passar o mouse no setBusca declarado no cardapio nos mostra seu tipo dispatch para declararmos na interface.
    
    Com o comando "npm install react-icons", instalamos para importar no buscador um ícone de busca para o componente, um cgSearch. Outro pacote útil para estilização com ícones em React.

--- ReactArquivosEstaticos/aluroni/src/pages/Cardapio/index.tsx
+++ ReactArquivosEstaticos/aluroni/src/pages/Cardapio/index.tsx
@@ -1,12 +1,23 @@
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
-  return (
+  const [busca, setBusca] = useState("");
+  // Para saber o tipo esperado do set antes de montar a interface Props do buscador, passa o mouse nele;
+  return ( 
     <main>
       <nav className={styles.menu}>
         <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+      </section>
     </main>
   );
 }
