4c22180c1ccf3d77b8e3b57eff76bb4dd9947aac
Lucas Burch <lucasburch@hotmail.com>
2022-08-08 11:40:22 -0300

    Alterações nos componentes
    Aula sobre alterar os componentes para que os inputs armazenem os dados em uma Array. Foi introduzido o useState para isso, de modo a fazer a aplicação re-renderizar o que vai sendo digitado nos inputs. O próximo passo é converter esse array nos cards de colaboradores do site.

--- ReactComJS/organo/src/App.js
+++ ReactComJS/organo/src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/index';
 import Formulario from './componentes/Formulario/index';
 // o index costuma ser pego direto da pasta, sem declarar, mas prefiro deixar visível.
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  } // espalha o array num novo com o colaborador recém cadastrado
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
     </div>
   );
 };
--- ReactComJS/organo/src/componentes/Formulario/index.js
+++ ReactComJS/organo/src/componentes/Formulario/index.js
@@ -2,8 +2,9 @@ import './Formulario.css';
 import CampoTexto from '../CampoTexto/index';
 import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
+import { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = (props) => {
 
   const times = [
     'Programação',
@@ -15,25 +16,60 @@ const Formulario = () => {
     'Inovação e Gestão'
   ];
 
+
+  const [nome, setNome] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [time, setTime] = useState('');
+  // Assim, atribuímos valores usando setValores do useState!! Pro React renderizar
+
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log('Form foi submetido');
-  }
+    props.aoColaboradorCadastrado({
+      nome, //igual a nome: nome etc...
+      cargo,
+      imagem,
+      time,
+    });
+  };
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} itens={times} label="Time" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={valor => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={valor => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          itens={times}
+          label="Time"
+          valor={time}
+          aoAlterado={valor => setTime(valor)}
+        />
         <Botao>
           Criar Card
         </Botao>
       </form>
     </section>
-  )
-}
+  );
+};
 
 export default Formulario;
