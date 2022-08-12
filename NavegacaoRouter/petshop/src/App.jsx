28f6cfcfccb93127771d681a5c9e6e64d7da2529
Lucas Burch <lucasburch@hotmail.com>
2022-08-12 17:17:17 -0300

    Criando Página 404 e componente Cabecalho
    Criação do cabeçalho para as páginas, encaixado no Router antes das Rotas, para aparecer em todas elas. Além disso, a Pagina404 ganhará redirecionamento para todos os paths inexistentes.

--- NavegacaoRouter/petshop/src/App.jsx
+++ NavegacaoRouter/petshop/src/App.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/Cabecalho';
 
 function App() {
 // exect impede que a '/' seja considerada no '/sobre';
-// para o React procurar SÓ UMA ROTA, usamos o componente Switch
+// para o React procurar SÓ UMA ROTA, usamos o componente Switch;
   return (
     <>
       <Router>
+        <Cabecalho />
         <Switch>
         <Route exact path='/'>
           <Home />
@@ -18,6 +21,10 @@ function App() {
 
         <Route path='/sobre'>
           <Sobre />
+        </Route>
+
+        <Route>
+          <Pagina404 />
         </Route>
         </Switch>
       </Router>
